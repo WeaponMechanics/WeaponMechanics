75291bbfead06498aceb3324b9d0515cf6199b84
Collin Barber <collinjbarber@gmail.com>
2025-08-10 13:33:30 -0400

    feat: allow item model in skins

--- weaponmechanics-core/src/main/java/me/deecaad/weaponmechanics/weapon/skin/BaseSkin.java
+++ weaponmechanics-core/src/main/java/me/deecaad/weaponmechanics/weapon/skin/BaseSkin.java
@@ -3,6 +3,7 @@ package me.deecaad.weaponmechanics.weapon.skin;
 import me.deecaad.core.file.SerializeData;
 import me.deecaad.core.file.Serializer;
 import me.deecaad.core.file.SerializerException;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ItemType;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -15,6 +16,7 @@ import java.util.OptionalInt;
 public class BaseSkin implements Skin, Serializer<BaseSkin> {
 
     private Optional<ItemType> type;
+    private Optional<NamespacedKey> itemModel;
     private OptionalInt customModelData;
 
     /**
@@ -25,13 +27,16 @@ public class BaseSkin implements Skin, Serializer<BaseSkin> {
 
     public BaseSkin(int customModelData) {
         this.type = Optional.empty();
+        this.itemModel = Optional.empty();
         this.customModelData = OptionalInt.of(customModelData);
     }
 
     public BaseSkin(
         @NotNull Optional<ItemType> type,
+        @NotNull Optional<NamespacedKey> itemModel,
         @NotNull OptionalInt customModelData) {
         this.type = type;
+        this.itemModel = itemModel;
         this.customModelData = customModelData;
     }
 
@@ -67,6 +72,11 @@ public class BaseSkin implements Skin, Serializer<BaseSkin> {
         if (meta == null)
             throw new IllegalArgumentException("Tried to apply skin to item without meta: " + weapon);
 
+        if (itemModel.isPresent() && (!meta.hasItemModel() || !meta.getItemModel().equals(itemModel.get()))) {
+            meta.setItemModel(itemModel.get());
+            hasMetaChanges = true;
+        }
+
         if (customModelData.isPresent() && (!meta.hasCustomModelData() || meta.getCustomModelData() != customModelData.getAsInt())) {
             meta.setCustomModelData(customModelData.getAsInt());
             hasMetaChanges = true;
@@ -80,14 +90,15 @@ public class BaseSkin implements Skin, Serializer<BaseSkin> {
     @Override
     public @NotNull BaseSkin serialize(@NotNull SerializeData data) throws SerializerException {
         Optional<ItemType> type = data.of("Type").getBukkitRegistry(ItemType.class);
+        Optional<NamespacedKey> itemModel = data.of("Item_Model").getNamespacedKey();
         OptionalInt customModelData = data.of("Custom_Model_Data").getInt();
 
         // Should use at least 1
-        if (type.isEmpty() && customModelData.isEmpty()) {
-            throw data.exception(null, "Tried to create a skin without using 'Type' or 'Custom_Model_Data'... did you misspell something?",
+        if (type.isEmpty() && itemModel.isEmpty() && customModelData.isEmpty()) {
+            throw data.exception(null, "Tried to create a skin without using 'Type' or 'Item_Model' or 'Custom_Model_Data'... did you misspell something?",
                 "Double check your configs to make sure it is spelled right.");
         }
 
-        return new BaseSkin(type, customModelData);
+        return new BaseSkin(type, itemModel, customModelData);
     }
 }
